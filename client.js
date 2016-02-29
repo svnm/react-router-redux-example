b9845057705140b84b2d548288b554118a84bb78
Steven Iseki Martin <stevenisekimartin@gmail.com>
2016-02-29 22:53:02 +1100

    updated to use useScroll

--- client.js
+++ client.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+import { Router, browserHistory, useRouterHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import useScroll from 'scroll-behavior/lib/useStandardScroll'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 import { configureStore } from './src/store'
 import routes from './src/routes'
 
 let state = window.__initialState__ || undefined
 const store = configureStore(browserHistory, state)
-const history = syncHistoryWithStore(browserHistory, store)
+const createScrollHistory = useScroll(createBrowserHistory)
+const appHistory = useRouterHistory(createScrollHistory)()
+const history = syncHistoryWithStore(appHistory, store)
 
 render(
   <Provider store={store}>
